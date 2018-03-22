700a096fc7bcc1058930b3f877eee37f9d01bd48
vbutkus <vytautasbutk@gmail.com>
2018-03-22 13:48:18 +0200

    initial commit

---
+++ wordpress-instaliavimas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wordpress instaliavimas</title>
+</head>
+<body>
+https://www.digitalocean.com/community/tutorials/how-to-install-wordpress-with-lamp-on-ubuntu-16-04
+
+cd /var/www/html
+sudo curl -O https://wordpress.org/latest.tar.gz
+sudo tar xzvf latest.tar.gz
+your.ip/wordpress
+</body>
+</html>
